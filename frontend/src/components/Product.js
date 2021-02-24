015a90b9ac445d603d82372fda052af7617b4f47
FazeelUsmani <fazeelusmani18@gmail.com>
2021-02-24 23:59:22 +0530

    added react-bootstrap dependency

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,37 +1,47 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+// import { Card } from 'react-bootstrap'
 
-function Product({product}) {
+function Product() {
     return (
         <div>
-        <Card className=" my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} />
-        </a>
-        Product     
-        
-        <Card.body>
-        <a href={`/product/${product._id}`}>
-        <Card.Title as="div">
-        <strong>{product.name}</strong>
-        </Card.Title>
-        </a>
-        <Card.text as="div">
-        <div className="my-3">
-        {product.rating} from {product.numReviews}
+            Product
         </div>
-        
-        </Card.text>
-        
-        <Card.text as="h3">
-        ${product.price}
-        </Card.text>
-        </Card.body>
-        
-        </Card>
-        </div>
-
     )
 }
 
 export default Product
+/*
+
+function Product({product}) {
+    return (
+    
+        <Card className=" my-3 p-3 rounded">
+            <a href={`/product/${product._id}`}>
+            <Card.Img src={product.image} />
+            </a>
+            Product     
+            
+            <Card.body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+                <Card.text as="div">
+                    <div className="my-3">
+                        {product.rating} from {product.numReviews}
+                    </div>
+                </Card.text>
+                
+                <Card.text as="h3">
+                    ${product.price}
+                </Card.text>
+            </Card.body>
+            
+        </Card>
+        
+    )
+}
+
+export default Product
+*/
